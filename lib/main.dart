c15483b1c4f4bcd46715e3561b46379a4b13c948
nhanpro <nhunguyenthanhnhu83@gmail.com>
2026-01-30 20:34:09 +0700

    tạo file khung cho file main và liên kết với file home để khi mở app lên trang home sẽ xuất hiện đầu tiên

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'screens/home/Home.dart';
+
+void main(){
+  runApp(const Main());
+}
+
+class Main extends StatelessWidget {
+  const Main({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHome(),
+    );
+  }
+}
