7867733a6b9ea37ea593fd9e4a5152bc805bf273
namnguyenit <97675663+namnguyenit@users.noreply.github.com>
2026-02-01 22:38:53 +0700

    Tạo khung giao diện đăng nhập

---
+++ lib/screens/auth/login_screen.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  final _formKey = GlobalKey<FormState>();
+
+  bool _isObscure = true;
+  bool _isLoading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Đăng nhập hệ thống")),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: _usernameController,
+                  decoration: const InputDecoration(
+                    labelText: 'Tên đăng nhập',
+                  ),
+                ),
+                TextFormField(
+                  controller: _passwordController,
+                  obscureText: _isObscure,
+                  decoration: InputDecoration(
+                    labelText: 'Mật khẩu',
+                    // prefixIcon: Icon(Icons.visibility_off),
+                    suffixIcon: IconButton(
+                      icon: Icon(_isObscure ? Icons.visibility : Icons.visibility_off) ,
+                      onPressed: () {
+                        setState(() {
+                          _isObscure = !_isObscure;
+                        });
+
+                      },
+
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 50,
+                  // child: ,
+                )
+              ],
+            )
+        ),
+      ),
+    );
+  }
+}
